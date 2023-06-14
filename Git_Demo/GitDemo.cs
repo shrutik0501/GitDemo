e78555602dd24086ab637b8f014d779301085b61
shrutik0501 <shrutik0501@gmail.com>
2023-06-14 14:03:49 +0530

    [Shruti]Add:Uploading UC1

---
+++ Git_Demo/GitDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    public class GitDemoClass
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Hi All");
+        }
+    }
+}
---
+++ Git_Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // UC1
+            GitDemoClass gitDemoClass = new GitDemoClass();
+            gitDemoClass.Demo();
+            Console.ReadLine();
+        }
+    }
+}
